93ac16eeff0a564d35ef0a11a45be8310f05d938
Viktoria <boychenkovika1985@gmail.com>
2026-04-29 15:43:04 +0200

    Added somr changes to the hw_3

--- hw_3/tasks.py
+++ hw_3/tasks.py
@@ -17,28 +17,37 @@ from sqlalchemy import create_engine, Column, Numeric, Boolean, ForeignKey, Inte
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-# engine = sqlalchemy.create_engine('sqlite:///:memory:')
-engine = sqlalchemy.create_engine('sqlite:///my_database.db')
 Base = declarative_base()
 
-# Создаем класс Session, который будет использоваться для взаимодействия с БД
-Session = sessionmaker(bind=engine)
-# Создаем экземпляр сессии
-session = Session()
 class Product(Base):
     __tablename__ = 'products'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     price = Column(Numeric(10,2))
     in_stock = Column(Boolean)
     category_id = Column(Integer, ForeignKey('categories.id'))
+
+    category = relationship('Category', back_populates='products')
+
+
 class Category(Base):
     __tablename__ = 'categories'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     description = Column(String(255))
+
     products = relationship('Product', back_populates='category')
-    Product.category = relationship('Category', back_populates='products')
+
+
+engine = sqlalchemy.create_engine('sqlite:///my_database.db')
+# engine = create_engine('sqlite:///:memory:')
+
+# Создаем класс Session, который будет использоваться для взаимодействия с БД
+Session = sessionmaker(bind=engine)
+# Создаем экземпляр сессии
+session = Session()
 
 # для проверки
 
